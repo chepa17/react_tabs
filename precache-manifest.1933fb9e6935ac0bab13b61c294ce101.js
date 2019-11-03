3c40f5f73f35bc7f561c632a81dc408135d12d85
Chepkyi Mykola <chepkiyn@gmail.com>
2019-11-03 14:36:10 +0200

    Updates

---
+++ precache-manifest.1933fb9e6935ac0bab13b61c294ce101.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f59bcab42a796ac9269a4b88b40577",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "56797b1c8eb326fe7c2f",
+    "url": "/react_tabs/static/css/main.8f8beba7.chunk.css"
+  },
+  {
+    "revision": "189e2b072e345d6c98d4",
+    "url": "/react_tabs/static/js/2.92f0ea7d.chunk.js"
+  },
+  {
+    "revision": "56797b1c8eb326fe7c2f",
+    "url": "/react_tabs/static/js/main.ba83dc03.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
